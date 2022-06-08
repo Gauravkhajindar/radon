7055e47e477236716521077560b81a717bfb1ac9
Gauravkhajindar <gauravkhajindar@gmail.com>
2022-06-08 14:54:22 +0530

    assignment mongo books and author

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,59 +1,61 @@
-const { count } = require("console")
-const BookModel= require("../models/bookModel")
 
-const createBook= async function (req, res) {
+const bookModel= require("../models/bookModel")
+const authorModel= require("../models/authorModel")
+// =========================createbook=============================================
+const createbook= async function (req, res) {
     let data= req.body
-
-    let savedData= await BookModel.create(data)
+    let savedData= await bookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+// ==========================createauthor=============================================
+const createauthor= async function (req, res) {
+    let data= req.body
+
+    let savedData= await authorModel.create(data)
+    res.send({msg: savedData})
+}
+// ========================getBooksbyChetanBhagat==============================================
+
+
+const getBooksbyChetanBhagat = async function (req,res){
+    let data = await authorModel.find({author_name:"Chetan Bhagat"}).select("author_id")
+    let bookData = await bookModel.find({author_id:data[0].author_id})
+    res.send({msg:bookData})
+}
+// ==========================authorOfBook==================================================
+
+const authorOfBook=async function(req,res){
+    let data=await bookModel.findOneAndUpdate({name:"Two states"},{$set:{price:100}},{new:true})
+     let authorname1=await authorModel.find({author_id:data.author_id}).select({author_Name:1, _id:0})
+     let price=data.price
+     res.send({msg:authorname1,price})
+} 
+
+// ==========================================================================
+
+const bookebetween50_100 =async function(req,res){
+    let data=await bookModel.find({price : { $gte: 50}  ,  price: {$lte: 100}}).select({author_id:1, _id:0})
+    const demo = data.map(inp => inp.author_id)
+    let temp = []
+
+    for(let i = 0; i < demo.length; i++){
+        let x = demo[i]
+        const author = await authorModel.find({author_id:x}).select({author_Name:1,author_id:1, _id:0})
+        temp.push(author)
+
+    }
+   
+    const author_Name = temp.flat()
+    res.send({msg:author_Name})
 }
 
 
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
-}
+module.exports.createbook= createbook
+module.exports.createauthor= createauthor
+module.exports.getBooksbyChetanBhagat= getBooksbyChetanBhagat
+module.exports.authorOfBook= authorOfBook
+module.exports.bookebetween50_100= bookebetween50_100
 
 
 
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,24 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+
+
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/createbook", BookController.createbook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post("/createauthor", BookController.createauthor  )
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
+router.get("/getBooksbyChetanBhagat", BookController.getBooksbyChetanBhagat)
 
-//MOMENT JS
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res) {
-    
-    // const today = moment();
-    // let x= today.add(10, "days")
+router.get("/authorOfBook", BookController.authorOfBook)
 
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+// router.get("/bookebetween50_100", BookController.bookebetween50_100)
 
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
-
-    res.send({ msg: "all good"})
-})
-
+router.get("/bookebetween50_100", BookController.bookebetween50_100)
 module.exports = router;
