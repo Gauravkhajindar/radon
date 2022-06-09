bcdbab0a47e8675f77aaa13d185d903cbd4ab9b1
Gauravkhajindar <gauravkhajindar@gmail.com>
2022-06-10 02:35:15 +0530

    assignment populate refernce

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,13 +2,21 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
-    name: String,
+    bookname: String,
     author_id: {
         type: ObjectId,
-        ref: "Author"
+        ref: "populateAuthor"
     },
     price: Number,
-    ratings: Number
+    ratings: Number,
+    publisher_id: {
+        type: ObjectId,
+        ref: "populatepublisher"
+    },
+    isHardCover :{
+        type:Boolean,
+        default:false
+    }
 
 
 }, { timestamps: true });
